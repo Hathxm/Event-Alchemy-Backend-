3f4bf18ca3787155b9e84e0c9f4463be96481391
hathim <hathim@mail.com>
2024-08-13 13:08:20 +0530

    asgi.py corrected

--- backend/asgi.py
+++ backend/asgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing  # Replace `chat` with your actual app name
+import chat.routing  
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
