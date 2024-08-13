b15f07408bd1b222be5709ea87b00f95576ddf4e
hathim <hathim@mail.com>
2024-08-13 14:10:34 +0530

    import chaged in asgi

--- backend/asgi.py
+++ backend/asgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat.routing import websocket_urlpatterns
+from chat import routing
 import django
 django.setup()
 
@@ -13,7 +13,7 @@ application = ProtocolTypeRouter({
     'http': get_asgi_application(),
     'websocket': AuthMiddlewareStack(
         URLRouter(
-            websocket_urlpatterns
+            routing.websocket_urlpatterns
         )
     ),
 })
