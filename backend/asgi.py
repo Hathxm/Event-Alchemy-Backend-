abb2344d90bb411d44b6b9aedd2663b07c9a825f
hathim <hathim@mail.com>
2024-08-13 14:32:44 +0530

    lines changed again

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,13 +1,13 @@
 import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat import routing
-import django
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-django.setup()
 
 
 application = ProtocolTypeRouter({
