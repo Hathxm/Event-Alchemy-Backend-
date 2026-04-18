0231e3031c1e979728b89fd1ceca44209613dde6
ESR656-MOHAMMED HATHIM <mhathim@ESRKOCHI.COM>
2026-04-18 22:03:47 +0530

    pip freeze and whitnoise middleware added

--- backend/settings.py
+++ backend/settings.py
@@ -73,6 +73,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware', 
+      'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
