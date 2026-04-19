b9843bbe1eecb89b1891eac9a6924704e6d4a09b
ESR656-MOHAMMED HATHIM <mhathim@ESRKOCHI.COM>
2026-04-19 21:00:34 +0530

    be url added to csrf origin

--- backend/settings.py
+++ backend/settings.py
@@ -278,6 +278,7 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "https://events.event-alchemy.fun",
-    "https://event-alchemy-frontend-vued.vercel.app"
+    "https://event-alchemy-frontend-vued.vercel.app",
+    "https://event-alchemy-backend-production.up.railway.app"
 ]
 
